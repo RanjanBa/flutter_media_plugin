dcdbcb548ac02cb54d715885b67bfbe2902f2071
RanjanBa <ranjanbaro2015@gmail.com>
2020-01-12 18:02:34 +0530

    changing playlist

---
+++ lib/media/media.dart
@@ -0,0 +1,19 @@
+abstract class Media {
+  String _key;
+  String _title;
+  String _url;
+
+  String get key => _key;
+  String get title => _title;
+  String get url => _url;
+
+  Map<String, dynamic> toJson() {
+    print("media.dart: Media to json");
+    return null;
+  }
+
+  factory Media.fromMap(Map<String, dynamic> map) {
+    print("media.dart: Media from map");
+    return null;
+  }
+}
---
+++ lib/media/video.dart
@@ -0,0 +1,18 @@
+import 'package:flutter_media_plugin/media/media.dart';
+
+class Video implements Media {
+
+  @override
+  String get key => null;
+
+  @override
+  String get title => null;
+
+  @override
+  Map<String, dynamic> toJson() {
+    return null;
+  }
+
+  @override
+  String get url => null;
+}
